d4333b4950ba5dae05ea6e8615a279074719c4a3
Jeffry Gonzalez <jeff@hypertheory.com>
2021-02-01 14:31:30 -0500

    got the list almost done

--- src/app/feature/communications/components/shopping-list/shopping-list.component.ts
+++ src/app/feature/communications/components/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingListItem } from '../../models';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
+  items: ShoppingListItem[] = [
+    { id: '1', description: 'Beer', purchased: false },
+    { id: '2', description: 'Ice Cream', purchased: true }
+  ];
   constructor() { }
 
   ngOnInit(): void {
