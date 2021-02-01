0c93fd42c9986a839c39cabd54fde9c1799020b5
Jeffry Gonzalez <jeff@hypertheory.com>
2021-02-01 16:41:31 -0500

    done with day 1

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable, ObservableInput } from 'rxjs';
+import { ShoppingDataService } from 'src/app/feature/communications/services/shopping-data.service';
+import { ShoppingDashboard } from 'src/app/models';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  stuff$: Observable<ShoppingDashboard>;
+  constructor(private shoppingService: ShoppingDataService) { }
 
   ngOnInit(): void {
+    this.stuff$ = this.shoppingService.getObservable()
+      .pipe(
+        map(items => {
+          return {
+            totalItems: items.length,
+            purchasedItems: items.filter(i => i.purchased === true).length,
+            unpurchasedItems: items.filter(i => i.purchased === false).length
+          } as ShoppingDashboard;
+        })
+      );
   }
 
 }
