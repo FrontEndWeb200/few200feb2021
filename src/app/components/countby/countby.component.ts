7761e46eb0c56ee8733391f7db4f24e26c92ea9e
Jeffry Gonzalez <jeff@hypertheory.com>
2021-02-02 15:15:37 -0500

    done with count by

--- src/app/components/countby/countby.component.ts
+++ src/app/components/countby/countby.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { countBySet } from 'src/app/actions/counter.actions';
-import { AppState } from 'src/app/reducers';
+import { AppState, selectCounterBy } from 'src/app/reducers';
 
 @Component({
   selector: 'app-countby',
@@ -10,9 +11,11 @@ import { AppState } from 'src/app/reducers';
 })
 export class CountbyComponent implements OnInit {
 
+  by$: Observable<number>;
   constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    this.by$ = this.store.select(selectCounterBy);
   }
 
   countBy(by: number): void {
