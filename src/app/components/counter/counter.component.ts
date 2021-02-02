d6aa834df260bdfa6f94d31640074152c224da71
Jeffry Gonzalez <jeff@hypertheory.com>
2021-02-02 12:15:13 -0500

    done with basic redux counter

--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AppState } from 'src/app/reducers';
 
 @Component({
   selector: 'app-counter',
@@ -7,18 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CounterComponent implements OnInit {
 
-  current = 0;
-  constructor() { }
+  current$: Observable<number>;
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    this.current$ = this.store
+      .pipe(
+        map(s => s.counter.current)
+      );
   }
 
   increment(): void {
-
+    this.store.dispatch({ type: 'increment' });
   }
 
   decrement(): void {
-
+    this.store.dispatch({ type: 'decrement' });
   }
 
 }
--- src/app/reducers/counter.reducer.ts
+++ src/app/reducers/counter.reducer.ts
@@ -11,5 +11,24 @@ const initialState: CounterState = {
 
 // reducer(currentState, action) => newState
 export function reducer(state: CounterState = initialState, action: Action): CounterState {
-  return state;
+  // this must be a PURE function
+  // you cannot change the arguments in any way (state, or the action)
+  // you can only produce a new value
+  // You cannot do "side effects" here.
+  //  - Side effects are things like calling APIs, changing a route, updating some other data, etc.
+
+  switch (action.type) {
+    case 'increment': {
+      return {
+        current: state.current + 1
+      };
+    }
+    case 'decrement': {
+      return {
+        current: state.current - 1
+      };
+    }
+    default:
+      return state;
+  }
 }
